7b4758ff65aded76b3248139b0cce8fca1b28a28
Bartlomiej Zimny <b.zimny@getprintbox.com>
2021-04-03 00:08:59 +0200

    fix problems

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { EmptyItem, Contener } from './components/StyledComponent';
 import Clock from './components/WorldClock';
