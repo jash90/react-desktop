ae61d433dff02b270385a1de3db053091e5870b8
Bartlomiej Zimny <b.zimny@getprintbox.com>
2021-04-04 05:28:11 +0200

    fix url

--- src/utils/Const.ts
+++ src/utils/Const.ts
@@ -1,3 +1,3 @@
-export const MARKET_STOCK_URL = "https://api.marketstack.com/v1/eod/latest?access_key=c1ecbf91d6505485821fc5a818fb7b94&symbols=";
+export const MARKET_STOCK_URL = "http://api.marketstack.com/v1/eod/latest?access_key=c1ecbf91d6505485821fc5a818fb7b94&symbols=";
 export const CURRENCY_URL = "https://api.nbp.pl/api/exchangerates/tables/A?format=json";
 export const CRYPTO_CURRENCY_URL = "https://api.binance.com/api/v3/ticker/price";
