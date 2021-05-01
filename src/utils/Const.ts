162fb6e9538158ae43296dd65c23e32feef964b8
Bartlomiej Zimny <b.zimny@getprintbox.com>
2021-05-01 23:34:32 +0200

    change etf endpoint

--- src/utils/Const.ts
+++ src/utils/Const.ts
@@ -2,7 +2,7 @@
 export const MARKET_STOCK_URL = "https://financialmodelingprep.com/api/v3/stock/list";
 export const CURRENCY_URL = "https://api.nbp.pl/api/exchangerates/tables/A/?format=json";
 export const CRYPTO_CURRENCY_URL = "https://api.binance.com/api/v1/ticker/24hr";
-export const ETF_URL = "https://financialmodelingprep.com/api/v3/quotes/etf";
+export const ETF_URL = "https://financialmodelingprep.com/api/v3/etf/list";
 
 //refreshTime
 export const REFRESH_TIME_60_MIN = 3600000;
